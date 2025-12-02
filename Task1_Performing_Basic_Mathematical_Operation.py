1052d83de4d6fb46fe2048ddcfe8fda3290ae1d7
saipavanb9-Tempest <saipavanbata200304@gmail.com>
2025-12-02 17:03:49 +0530

    Commit Message

---
+++ Task1_Performing_Basic_Mathematical_Operation.py
@@ -0,0 +1,16 @@
+"""
+Performing Basic Mathematical Operations
+Addition, Subtraction, Multiplication, Division
+"""
+
+Num1 = float(input("Enter the first number: "))
+Num2 = float(input("Enter the second number: "))
+
+Addition = Num1+Num2
+Subtraction = Num1-Num2
+Multiplication = Num1*Num2
+Division = Num1/Num2
+print("Addition: ", round(Addition,2))
+print("Subtraction: ",round(Subtraction,2))
+print("Multiplication: ", round(Multiplication,2))
+print("Division: ", round(Division,2))
